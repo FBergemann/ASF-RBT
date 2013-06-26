68d33262810390c8a9190b72901c8625487c017d
FBergemann <frank67x@gmail.com>
2013-06-26 09:25:54 +0300

    Update and rename main.cpp to asf_rbtree.h
    extract main from RBT impl and rename properly

--- asf_rbtree.h
+++ asf_rbtree.h
@@ -13,9 +13,6 @@
 #include <iostream>
 #include <iomanip>
 
-#define VERIFY_RBTREE
-#define TRACE
-
 // TODO: add support for rvalue references
 template <typename KEY, typename VALUE>
 struct RbtNode {
@@ -564,64 +561,3 @@ struct RbtNode {
 
 };
 
-int main(
-	int,
-	char**)
-{
-	{
-		RbtNode<int, int>::Tree tree;
-
-		for(int i=0; i<20; i++)
-		{
-			int x = rand() % 10000;
-			int y = rand() % 10000;
-			// printf("Inserting %d -> %d\n\n", x, y);
-			tree.insert(x, y);
-			// assert(tree->lookup(x) == y);
-			// tree.print();
-		}
-		tree.print();
-	}
-	{
-		RbtNode<int, int>::Tree tree;
-
-		for(int i=0; i<20; i++)
-		{
-			int x = i;
-			int y = i;
-			// printf("Inserting %d -> %d\n\n", x, y);
-			tree.insert(x, y);
-			// assert(tree->lookup(x) == y);
-			// tree.print();
-		}
-		tree.print();
-	}
-	{
-		RbtNode<int, int>::Tree tree;
-
-		for(int i=0; i<20; i++)
-		{
-			int x = 19-i;
-			int y = 19-i;
-			// printf("Inserting %d -> %d\n\n", x, y);
-			tree.insert(x, y);
-			// assert(tree->lookup(x) == y);
-			// tree.print();
-		}
-		tree.print();
-	}
-
-#if 0
-	for (int i=0; i<60000; i++)
-	{
-		int x = rand() % 10000;
-#ifdef TRACE
-		print_tree(t);
-		printf("Deleting key %d\n\n", x);
-#endif
-		rbtree_delete(t, (void*)x, compare_int);
-	}
-#endif
-
-	return 0;
-}
